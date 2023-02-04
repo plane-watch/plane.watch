f3f0b81d20948ff77dca676bceadd9651b19263e
Jason Playne <jason@jasonplayne.com>
2023-02-04 15:52:48 +0800

    use a SQL Windowing function to get 1 update every 10 seconds from the _low feed when returning plane history

--- cmd/pw_ws_broker/clickhouse.go
+++ cmd/pw_ws_broker/clickhouse.go
@@ -29,12 +29,17 @@ func NewClickHouseData(url string) (*ClickHouseData, error) {
 }
 
 func (chd *ClickHouseData) PlaneLocationHistory(icao, callSign string) []ws_protocol.LocationHistory {
-	history := make([]ws_protocol.LocationHistory, 0, 1000)
-	query := "SELECT DISTINCT Lat, Lon, Velocity, Altitude, Heading " +
-		"FROM location_updates_low " +
-		"WHERE Icao = $1 AND CallSign = $2 AND HasLocation = 1 AND TileLocation != 'tileUnknown' " +
-		"AND LastMsg > toStartOfInterval(NOW(), INTERVAL 6 HOUR) " +
-		"ORDER BY LastMsg"
+	history := make([]ws_protocol.LocationHistory, 0, 2200) // if we increase from 6 hours, increase our initial allocation
+	query := `WITH t AS (
+SELECT *
+FROM location_updates_low
+WHERE Icao = '` + icao + `' AND CallSign = '` + callSign + `' AND HasLocation = 1 AND TileLocation != 'tileUnknown'
+  AND LastMsg > toStartOfInterval(NOW(), INTERVAL 6 HOUR)
+), t_over AS (
+    SELECT *, ROW_NUMBER() OVER(PARTITION BY toInt64(toInt64(LastMsg)/10) ORDER BY LastMsg) AS N FROM t
+)
+
+SELECT Lat, Lon, Velocity, Altitude, Heading FROM t_over where N=1 ORDER BY LastMsg`
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
