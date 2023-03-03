8687dc40104f9c8c64381982563685ab67eac66c
Jason Playne <jason@jasonplayne.com>
2023-03-03 17:50:51 +0800

    some unit tests for Merging CallSigns

--- lib/export/types_test.go
+++ lib/export/types_test.go
@@ -1,9 +1,15 @@
 package export
 
 import (
+	"github.com/rs/zerolog"
 	"testing"
 	"time"
 )
+
+func TestMain(m *testing.M) {
+	zerolog.SetGlobalLevel(zerolog.Disabled)
+	m.Run()
+}
 
 func TestIsLocationPossible(t *testing.T) {
 	msg1t := time.Date(2023, time.January, 9, 19, 0, 0, 0, time.Local)
@@ -22,5 +28,101 @@ func TestIsLocationPossible(t *testing.T) {
 
 	if IsLocationPossible(pos2, pos1) {
 		t.Error("Pos2 -> Pos1 is not possible")
+	}
+}
+
+func TestMergeCallSign(t *testing.T) {
+	type args struct {
+		prev PlaneLocation
+		next PlaneLocation
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    PlaneLocation
+		wantErr bool
+	}{
+		{
+			name: "both-filled",
+			args: args{
+				prev: PlaneLocation{CallSign: ptr("ONE")},
+				next: PlaneLocation{CallSign: ptr("TWO")},
+			},
+			want:    PlaneLocation{CallSign: ptr("TWO")},
+			wantErr: false,
+		},
+		{
+			name: "One-Blank",
+			args: args{
+				prev: PlaneLocation{CallSign: ptr("")},
+				next: PlaneLocation{CallSign: ptr("TWO")},
+			},
+			want:    PlaneLocation{CallSign: ptr("TWO")},
+			wantErr: false,
+		},
+		{
+			name: "Two-Blank",
+			args: args{
+				prev: PlaneLocation{CallSign: ptr("ONE")},
+				next: PlaneLocation{CallSign: ptr("")},
+			},
+			want:    PlaneLocation{CallSign: ptr("ONE")},
+			wantErr: false,
+		},
+		{
+			name: "One-nil",
+			args: args{
+				prev: PlaneLocation{CallSign: nil},
+				next: PlaneLocation{CallSign: ptr("")},
+			},
+			want:    PlaneLocation{CallSign: ptr("")},
+			wantErr: false,
+		},
+		{
+			name: "Two-nil",
+			args: args{
+				prev: PlaneLocation{CallSign: ptr("ONE")},
+				next: PlaneLocation{CallSign: nil},
+			},
+			want:    PlaneLocation{CallSign: ptr("ONE")},
+			wantErr: false,
+		},
+		{
+			name: "both-nil",
+			args: args{
+				prev: PlaneLocation{CallSign: nil},
+				next: PlaneLocation{CallSign: nil},
+			},
+			want:    PlaneLocation{CallSign: nil},
+			wantErr: false,
+		},
+		{
+			name: "both-blank",
+			args: args{
+				prev: PlaneLocation{CallSign: ptr("")},
+				next: PlaneLocation{CallSign: ptr("")},
+			},
+			want:    PlaneLocation{CallSign: ptr("")},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := MergePlaneLocations(tt.args.prev, tt.args.next)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("MergePlaneLocations() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if nil != tt.want.CallSign {
+				if unPtr(got.CallSign) != unPtr(tt.want.CallSign) {
+					t.Errorf("MergePlaneLocations() got = %s, want %s", unPtr(got.CallSign), unPtr(tt.want.CallSign))
+				}
+			} else {
+				if got.CallSign != nil {
+					t.Errorf("Expected a nil callsign, got %s", unPtr(got.CallSign))
+				}
+			}
+		})
 	}
 }
